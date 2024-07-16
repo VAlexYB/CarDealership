cff9d463c2f5e9fe427775d514c2a915be4e5045
DESKTOP-KCBE42H\vasal <vasalexdds@yandex.ru>
2024-07-16 18:38:33 +0300

    Добавил чутка тестов на репозиторий

---
+++ CarDealership.Tests/RepositoriesTests/AutoConfigsRepositoryTests.cs
@@ -0,0 +1,291 @@
+﻿using CarDealership.Core.Filters;
+using CarDealership.Core.Models;
+using CarDealership.DataAccess.Entities;
+using CarDealership.DataAccess.Factories;
+using CarDealership.DataAccess.Repositories;
+using CarDealership.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+
+namespace CarDealership.Tests.RepositoriesTests
+{
+    public class AutoConfigsRepositoryTests : IDisposable
+    {
+        private readonly Mock<IDistributedCache> _mockCache;
+        private readonly DbContextOptions<CarDealershipDbContext> _dbContextOptions;
+        private readonly AutoConfigsRepository _repository;
+        private readonly Mock<IEntityModelFactory<AutoConfiguration, AutoConfigurationEntity>> _mockFactory;
+
+        public AutoConfigsRepositoryTests()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<CarDealershipDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) 
+                .Options;
+
+            _mockCache = new Mock<IDistributedCache>();
+            _mockFactory = new Mock<IEntityModelFactory<AutoConfiguration, AutoConfigurationEntity>>();
+            _repository = new AutoConfigsRepository(new CarDealershipDbContext(_dbContextOptions), _mockFactory.Object, _mockCache.Object);
+        }
+
+        public void Dispose()
+        {
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+
+        [Fact]
+        public async Task UpdateAsync_UpdatesAutoConfiguration_WhenExists()
+        {
+            // Arrange
+            var autoConfigId = Guid.NewGuid();
+            var existingEntity = new AutoConfigurationEntity
+            {
+                Id = autoConfigId,
+                AutoModelId = Guid.NewGuid(),
+                BodyTypeId = Guid.NewGuid(),
+                DriveTypeId = Guid.NewGuid(),
+                EngineId = Guid.NewGuid(),
+                ColorId = Guid.NewGuid(),
+                EquipmentId = Guid.NewGuid(),
+                IsDeleted = false
+            };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.AutoConfigurations.Add(existingEntity);
+                await context.SaveChangesAsync();
+            }
+
+            var updatedConfig = AutoConfiguration.Create(
+                autoConfigId,
+                1000,
+                existingEntity.AutoModelId,
+                existingEntity.BodyTypeId,
+                Guid.NewGuid(), // Change one property for testing
+                existingEntity.EngineId,
+                existingEntity.ColorId,
+                existingEntity.EquipmentId
+            ).Value;
+
+            _mockFactory.Setup(f => f.CreateEntity(updatedConfig))
+                .Returns(new AutoConfigurationEntity
+                {
+                    Id = updatedConfig.Id,
+                    AutoModelId = updatedConfig.AutoModelId,
+                    BodyTypeId = updatedConfig.BodyTypeId,
+                    DriveTypeId = updatedConfig.DriveTypeId,
+                    EngineId = updatedConfig.EngineId,
+                    ColorId = updatedConfig.ColorId,
+                    EquipmentId = updatedConfig.EquipmentId
+                });
+
+            // Act
+            var result = await _repository.UpdateAsync(updatedConfig);
+
+            // Assert
+            Assert.Equal(autoConfigId, result);
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                var updatedEntity = await context.AutoConfigurations.FindAsync(autoConfigId);
+                Assert.Equal(updatedConfig.DriveTypeId, updatedEntity.DriveTypeId);
+            }
+        }
+
+        [Fact]
+        public async Task GetFilteredAsync_ReturnsFilteredAutoConfigurations()
+        {
+            // Arrange
+            var autoConfig1 = new AutoConfigurationEntity
+            { 
+                Id = Guid.NewGuid(),
+                AutoModelId = Guid.NewGuid(),
+                BodyTypeId = Guid.NewGuid(),
+                DriveTypeId = Guid.NewGuid(),
+                EngineId = Guid.NewGuid(),
+                ColorId = Guid.NewGuid(),
+                EquipmentId = Guid.NewGuid(),
+                IsDeleted = false 
+            };
+            var autoConfig2 = new AutoConfigurationEntity {
+                Id = Guid.NewGuid(),
+                AutoModelId = autoConfig1.AutoModelId,
+                BodyTypeId = Guid.NewGuid(),
+                DriveTypeId = Guid.NewGuid(),
+                EngineId = Guid.NewGuid(),
+                ColorId = Guid.NewGuid(),
+                EquipmentId = Guid.NewGuid(),
+                IsDeleted = false 
+            };
+            var autoConfig3 = new AutoConfigurationEntity
+            {
+                Id = Guid.NewGuid(),
+                AutoModelId = Guid.NewGuid(),
+                BodyTypeId = Guid.NewGuid(),
+                DriveTypeId = Guid.NewGuid(),
+                EngineId = Guid.NewGuid(),
+                ColorId = Guid.NewGuid(),
+                EquipmentId = Guid.NewGuid(),
+                IsDeleted = false
+            };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.AutoConfigurations.AddRange(autoConfig1, autoConfig2, autoConfig3);
+                await context.SaveChangesAsync();
+            }
+
+            var filter = new ConfigurationsFilter { AutoModelId = autoConfig1.AutoModelId };
+
+            _mockFactory.Setup(f => f.CreateModel(It.IsAny<AutoConfigurationEntity>()))
+                .Returns((AutoConfigurationEntity entity) => 
+                AutoConfiguration.Create(
+                    entity.Id,
+                    1000,
+                    entity.AutoModelId,
+                    entity.BodyTypeId,
+                    entity.DriveTypeId,
+                    entity.EngineId,
+                    entity.ColorId,
+                    entity.EquipmentId
+                ).Value);
+
+            // Act
+            var result = await _repository.GetFilteredAsync(filter);
+
+            // Assert
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ReturnsAllAutoConfigurations()
+        {
+            // Arrange
+            var autoConfig1 = new AutoConfigurationEntity
+            {
+                Id = Guid.NewGuid(),
+                AutoModelId = Guid.NewGuid(),
+                BodyTypeId = Guid.NewGuid(),
+                DriveTypeId = Guid.NewGuid(),
+                EngineId = Guid.NewGuid(),
+                ColorId = Guid.NewGuid(),
+                EquipmentId = Guid.NewGuid(),
+                IsDeleted = false
+            };
+            var autoConfig2 = new AutoConfigurationEntity
+            {
+                Id = Guid.NewGuid(),
+                AutoModelId = Guid.NewGuid(),
+                BodyTypeId = Guid.NewGuid(),
+                DriveTypeId = Guid.NewGuid(),
+                EngineId = Guid.NewGuid(),
+                ColorId = Guid.NewGuid(),
+                EquipmentId = Guid.NewGuid(),
+                IsDeleted = false
+            };
+            var autoConfig3 = new AutoConfigurationEntity
+            {
+                Id = Guid.NewGuid(),
+                AutoModelId = Guid.NewGuid(),
+                BodyTypeId = Guid.NewGuid(),
+                DriveTypeId = Guid.NewGuid(),
+                EngineId = Guid.NewGuid(),
+                ColorId = Guid.NewGuid(),
+                EquipmentId = Guid.NewGuid(),
+                IsDeleted = true // удаленная конфигурация
+            };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.AutoConfigurations.AddRange(autoConfig1, autoConfig2, autoConfig3);
+                await context.SaveChangesAsync();
+            }
+
+            _mockFactory.Setup(f => f.CreateModel(It.IsAny<AutoConfigurationEntity>()))
+                .Returns((AutoConfigurationEntity entity) => AutoConfiguration.Create(entity.Id, 1000, entity.AutoModelId, entity.BodyTypeId, entity.DriveTypeId, entity.EngineId, entity.ColorId, entity.EquipmentId).Value);
+
+            // Act
+            var result = await _repository.GetAllAsync();
+
+            // Assert
+            Assert.Equal(2, result.Count); // 2 должны вернуться, т.к.  1 из 3 удалена
+        }
+
+
+        [Fact]
+        public async Task InsertAsync_AddsNewAutoConfiguration()
+        {
+            // Arrange
+            var newConfig = AutoConfiguration.Create(Guid.NewGuid(), 1000, Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()).Value;
+            _mockFactory.Setup(f => f.CreateEntity(newConfig))
+                .Returns(new AutoConfigurationEntity { Id = newConfig.Id, AutoModelId = newConfig.AutoModelId });
+
+            // Act
+            var result = await _repository.InsertAsync(newConfig);
+
+            // Assert
+            Assert.Equal(newConfig.Id, result);
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                var addedEntity = await context.AutoConfigurations.FindAsync(newConfig.Id);
+                Assert.NotNull(addedEntity);
+            }
+        }
+
+        [Fact]
+        public async Task DeleteAsync_SoftDeletesAutoConfiguration()
+        {
+            // Arrange
+            var autoConfigId = Guid.NewGuid();
+            var autoConfigEntity = new AutoConfigurationEntity { Id = autoConfigId, AutoModelId = Guid.NewGuid(), IsDeleted = false };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.AutoConfigurations.Add(autoConfigEntity);
+                await context.SaveChangesAsync();
+            }
+
+            // Act
+            await _repository.DeleteAsync(autoConfigId);
+
+            // Assert
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                var deletedEntity = await context.AutoConfigurations.FindAsync(autoConfigId);
+                Assert.True(deletedEntity.IsDeleted);
+            }
+        }
+
+        [Fact]
+        public async Task ExistsAsync_ReturnsTrue_WhenAutoConfigurationExists()
+        {
+            // Arrange
+            var autoConfigId = Guid.NewGuid();
+            var autoConfigEntity = new AutoConfigurationEntity { Id = autoConfigId, AutoModelId = Guid.NewGuid(), IsDeleted = false };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.AutoConfigurations.Add(autoConfigEntity);
+                await context.SaveChangesAsync();
+            }
+
+            // Act
+            var exists = await _repository.ExistsAsync(autoConfigId);
+
+            // Assert
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public async Task ExistsAsync_ReturnsFalse_WhenAutoConfigurationDoesNotExist()
+        {
+            // Act
+            var exists = await _repository.ExistsAsync(Guid.NewGuid());
+
+            // Assert
+            Assert.False(exists);
+        }
+    }
+}
---
+++ CarDealership.Tests/RepositoriesTests/BodyTypesRepositoryTests.cs
@@ -0,0 +1,194 @@
+﻿using CarDealership.Core.Models;
+using CarDealership.DataAccess.Entities;
+using CarDealership.DataAccess.Factories;
+using CarDealership.DataAccess.Repositories;
+using CarDealership.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace CarDealership.Tests.RepositoriesTests
+{
+    public class BodyTypesRepositoryTests : IDisposable
+    {
+        private readonly Mock<IDistributedCache> _mockCache;
+        private readonly DbContextOptions<CarDealershipDbContext> _dbContextOptions;
+        private readonly BodyTypesRepository _repository;
+        private readonly Mock<IEntityModelFactory<BodyType, BodyTypeEntity>> _mockFactory;
+
+        public BodyTypesRepositoryTests()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<CarDealershipDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) 
+                .Options;
+
+            _mockCache = new Mock<IDistributedCache>();
+            _mockFactory = new Mock<IEntityModelFactory<BodyType, BodyTypeEntity>>();
+            var context = new CarDealershipDbContext(_dbContextOptions);
+            _repository = new BodyTypesRepository(context, _mockFactory.Object, _mockCache.Object);
+        }
+
+        public void Dispose()
+        {
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.Database.EnsureDeleted(); 
+            }
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ReturnsAllBodyTypes()
+        {
+            // Arrange
+            var bodyTypes = new List<BodyTypeEntity>
+        {
+            new BodyTypeEntity { Id = Guid.NewGuid(), Value = "Sedan", IsDeleted = false },
+            new BodyTypeEntity { Id = Guid.NewGuid(), Value = "SUV", IsDeleted = false }
+        };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.BodyTypes.AddRange(bodyTypes);
+                await context.SaveChangesAsync();
+            }
+
+            _mockFactory.Setup(f => f.CreateModel(It.IsAny<BodyTypeEntity>()))
+                .Returns((BodyTypeEntity entity) => BodyType.Create(entity.Id, entity.Value, 1000).Value);
+
+            // Act
+            var result = await _repository.GetAllAsync();
+
+            // Assert
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ReturnsBodyType_WhenExists()
+        {
+            // Arrange
+            var bodyTypeId = Guid.NewGuid();
+            var bodyTypeEntity = new BodyTypeEntity { Id = bodyTypeId, Value = "Hatchback", IsDeleted = false };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.BodyTypes.Add(bodyTypeEntity);
+                await context.SaveChangesAsync();
+            }
+
+            _mockFactory.Setup(f => f.CreateModel(It.IsAny<BodyTypeEntity>()))
+                .Returns((BodyTypeEntity entity) => BodyType.Create(entity.Id, entity.Value, 2000).Value);
+
+            // Act
+            var result = await _repository.GetByIdAsync(bodyTypeId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(bodyTypeId, result.Id);
+        }
+
+        [Fact]
+        public async Task InsertAsync_AddsNewBodyType()
+        {
+            // Arrange
+            var bodyType = BodyType.Create(Guid.NewGuid(), "Coupe", 1000).Value;
+
+            _mockFactory.Setup(f => f.CreateEntity(bodyType))
+                .Returns(new BodyTypeEntity { Id = bodyType.Id, Value = bodyType.Value, IsDeleted = false });
+
+            // Act
+            var result = await _repository.InsertAsync(bodyType);
+
+            // Assert
+            Assert.Equal(bodyType.Id, result);
+
+            // Verify the body type was added
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                Assert.Equal(1, await context.BodyTypes.CountAsync());
+            }
+        }
+
+        [Fact]
+        public async Task UpdateAsync_UpdatesExistingBodyType()
+        {
+            // Arrange
+            var bodyTypeId = Guid.NewGuid();
+            var bodyTypeEntity = new BodyTypeEntity { Id = bodyTypeId, Value = "Convertible", IsDeleted = false };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.BodyTypes.Add(bodyTypeEntity);
+                await context.SaveChangesAsync();
+            }
+
+            var updatedBodyType = BodyType.Create(bodyTypeId, "Updated", 1000).Value;
+            _mockFactory.Setup(f => f.CreateEntity(updatedBodyType))
+                .Returns(new BodyTypeEntity { Id = bodyTypeId, Value = updatedBodyType.Value, IsDeleted = false });
+
+            // Act
+            await _repository.UpdateAsync(updatedBodyType);
+
+            // Assert
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                var updatedEntity = await context.BodyTypes.FindAsync(bodyTypeId);
+                Assert.Equal("Updated", updatedEntity.Value);
+            }
+        }
+
+        [Fact]
+        public async Task DeleteAsync_SoftDeletesBodyType()
+        {
+            // Arrange
+            var bodyTypeId = Guid.NewGuid();
+            var bodyTypeEntity = new BodyTypeEntity { Id = bodyTypeId, Value = "Minivan", IsDeleted = false };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.BodyTypes.Add(bodyTypeEntity);
+                await context.SaveChangesAsync();
+            }
+
+            // Act
+            await _repository.DeleteAsync(bodyTypeId);
+
+            // Assert
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                var deletedEntity = await context.BodyTypes.FindAsync(bodyTypeId);
+                Assert.True(deletedEntity.IsDeleted);
+            }
+        }
+
+        [Fact]
+        public async Task ExistsAsync_ReturnsTrue_WhenBodyTypeExists()
+        {
+            // Arrange
+            var bodyTypeId = Guid.NewGuid();
+            var bodyTypeEntity = new BodyTypeEntity { Id = bodyTypeId, Value = "Pickup", IsDeleted = false };
+
+            using (var context = new CarDealershipDbContext(_dbContextOptions))
+            {
+                context.BodyTypes.Add(bodyTypeEntity);
+                await context.SaveChangesAsync();
+            }
+
+            // Act
+            var exists = await _repository.ExistsAsync(bodyTypeId);
+
+            // Assert
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public async Task ExistsAsync_ReturnsFalse_WhenBodyTypeDoesNotExist()
+        {
+            // Act
+            var exists = await _repository.ExistsAsync(Guid.NewGuid());
+
+            // Assert
+            Assert.False(exists);
+        }
+    }
+}
